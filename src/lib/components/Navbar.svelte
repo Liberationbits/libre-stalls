6a7b11d45b154f4e32f5d8d2d4e2c3c0c1ee2cdc
rodant <antonio.rodriguez@posteo.de>
2023-10-24 12:07:32 +0200

    Restructure app towards libre stalls.

--- src/lib/components/Navbar.svelte
+++ src/lib/components/Navbar.svelte
@@ -4,55 +4,13 @@
 	import { notificationsEnabled } from '$stores/notifications';
 	import CurrentUser from './CurrentUser.svelte';
 	import NotificationButton from './NotificationButton.svelte';
-	import { MagicWand, PersonSimpleRun, Rss, Sparkle, ShoppingBagOpen } from 'phosphor-svelte';
+	import { MagicWand, ShoppingCart, HouseSimple, ShoppingBagOpen } from 'phosphor-svelte';
 </script>
 
 <div class="navbar mb-2 bg-base-100">
 	<div class="navbar-start">
-		<div class="dropdown ">
-			<label tabindex="0" class="btn btn-ghost xl:hidden">
-				<svg
-					xmlns="http://www.w3.org/2000/svg"
-					class="h-5 w-5"
-					fill="none"
-					viewBox="0 0 24 24"
-					stroke="currentColor"
-					><path
-						stroke-linecap="round"
-						stroke-linejoin="round"
-						stroke-width="2"
-						d="M4 6h16M4 12h8m-8 6h16"
-					/></svg
-				>
-			</label>
-			<ul
-				tabindex="0"
-				class="menu dropdown-content rounded-box menu-sm z-[1] mt-3 w-52 bg-base-100 p-2 shadow"
-			>
-				<li>
-					<a class:tab-active={$page.url.pathname === '/jobs'} class="" href="/jobs"
-						>Job Request Feed</a
-					>
-				</li>
-				<li>
-					<a class:tab-active={$page.url.pathname === '/dvms'} class="" href="/dvms">List DVMs</a>
-				</li>
-				{#if $currentUser}
-					<li>
-						<a class:tab-active={$page.url.pathname === '/jobs/my'} class="" href="/jobs/my"
-							>Your Jobs</a
-						>
-					</li>
-				{/if}
-				<li>
-					<a class:tab-active={$page.url.pathname === '/jobs/new'} class="" href="/jobs/new"
-						>Create Job Chain</a
-					>
-				</li>
-			</ul>
-		</div>
 		<a href="/" class="btn btn-ghost text-xl normal-case px-0 hover:bg-transparent">
-			<ShoppingBagOpen class="h-12 w-12" />
+			<ShoppingCart class="h-12 w-12" />
 			<h1 class="text-base md:text-xl font-bold">Freie-Shops</h1>
 		</a>
 	</div>
@@ -60,7 +18,7 @@
 	<div class="navbar-center hidden flex-row gap-8 xl:flex">
 		<ul class="menu menu-horizontal !bg-transparent">
 			<li>
-				<a class:tab-active={$page.url.pathname === '/magic'} class="" href="/magic">
+				<a class:tab-active={$page.url.pathname === '/paradigm'} class="" href="/paradigm">
 					<MagicWand class="h-6 w-6" />
 					Paradigmenwechsel</a
 				>
@@ -69,33 +27,26 @@
 
 		<ul class="menu menu-horizontal !bg-transparent">
 			<li>
-				<a class:tab-active={$page.url.pathname === '/jobs'} class="" href="/jobs">
-					<Rss class="h-6 w-6" />
-					Deine Bestellungen</a
-				>
-			</li>
-			<li>
-				<a class:tab-active={$page.url.pathname === '/dvms'} class="" href="/dvms">
-					<Sparkle class="h-6 w-6" />
-					Shops
+				<a class:tab-active={$page.url.pathname === '/stalls'} class="" href="/stalls">
+					<HouseSimple class="h-6 w-6" />
+					Läden
 				</a>
 			</li>
+		</ul>
+
+		<ul class="menu menu-horizontal !bg-transparent">
 			{#if $currentUser}
 				<li>
-					<a class:tab-active={$page.url.pathname === '/jobs/my'} class="" href="/jobs/my">
-						<PersonSimpleRun class="h-6 w-6" />
-						Your Jobs
-					</a>
+					<a class:tab-active={$page.url.pathname === '/orders'} class="" href="/orders">
+						<ShoppingBagOpen class="h-6 w-6" />
+						Deine Bestellungen</a
+					>
 				</li>
 			{/if}
 		</ul>
-
-		<a href="/jobs/new" class="btn btn-accent btn-outline ml-12 !rounded-full !py-1">
-			Kategorien
-		</a>
 	</div>
 	<div class="navbar-end flex flex-row items-center gap-4">
-		{#if $notificationsEnabled}
+		{#if $notificationsEnabled && $currentUser}
 			<NotificationButton />
 		{/if}
 
---
+++ src/routes/paradigm/+page.svelte
@@ -0,0 +1,64 @@
+<script lang="ts">
+</script>
+
+<div class="max-w-5xl mx-auto flex flex-col gap-8">
+    <div class="flex flex-row gap-6 items-center">
+        <img
+			src="/favicon.png"
+			alt="Stall"
+			class="rounded-box h-full logo"
+		/>
+        <div class="flex flex-col gap-2">
+            <h1 class="text-7xl font-black tracking-wide">Paradigmenwechsel</h1>
+            <div class="text-lg text-orange-400">
+                Es gibt einige Dinge, die Freie-Shops zu etwas einzigartig machen.
+            </div>
+        </div>
+    </div>
+
+    <div class="grid grid-cols-2 gap-6">
+        <div class="card card-compact rounded-box">
+            <div class="card-body !text-base">
+                <div class="card-title text-base-100-content font-bold">
+                    Offene Standards
+                </div>
+
+                <p>- Rückkehr zu den ursprünglichen Pronzipien des Webs</p>
+
+                <p>- Der Marktplatz richtet sich nach Community-Standards, die von verschiedenen Entitäten implementiert werden können</p>
+
+                <p>- Ein bestimmter Marktplatz ist dadurch austauschbar, es entsteht eine fairer Wettbewerb</p>
+            </div>
+        </div>
+
+        <div class="card card-compact rounded-box">
+            <div class="card-body !text-base">
+                <div class="card-title text-base-100-content font-bold">
+                    Hohheit über die Daten
+                </div>
+
+                <p>Die Daten der Shops, der Produkte, der Kunden und deren Interaktionen sind vom Marktplatz getrennt und liegen in der Obhut der Marktteilnehmer.</p>
+
+                <p></p>
+            </div>
+        </div>
+
+        <div class="card card-compact rounded-box">
+            <div class="card-body !text-base">
+                <div class="card-title text-base-100-content font-bold">
+                    Dezentralität
+                </div>
+
+                <p>Eine dezentrale Architektur gewährleistet für die Marktteilnehmer die freie Wahl der Service-Provider. Gleichzeitig ist eine Behinderung oder 
+                    Ausschluss eines Teilnehmers nicht möglich.</p>
+            </div>
+        </div>
+    </div>
+</div>
+
+<style>
+    .logo {
+        width: 209px;
+        height: 209px;
+    }
+</style>
